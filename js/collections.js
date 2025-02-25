1ae956fe268618131505795384fe819dd487184f
blairajohnson <33012150+blairajohnson@users.noreply.github.com>
2025-02-24 21:14:54 -0500

    updating collections list
    as of 2/24

--- js/collections.js
+++ js/collections.js
@@ -4,190 +4,274 @@ let collections = [
         author:"Hue",
         range:"2019 – present",
         link:"https://www.are.na/hue-ho_8g3p7r7y/practicing-compassionate-curiosity",
-        method:""
+        method:"I read something and it sparks, or I’ll let my mind meander and words form. Sometimes it’s overhearing it. Sometimes it’s through conversation with friends and brief encounters. I use a website called Are.na, and there’s a feature that lets you toggle your “channels” to let others add questions. So sometimes people add their own questions or connect the questions I’ve collected into their own channels."
     },
     {
         title:"A collection of security patterns found in envelope liners",
         author:"Brandon Alvarado",
         range:"2018 – present",
         link:"https://www.are.na/brandon-alvarado/envelope-liners",
-        method:""
+        method:"I keep a physical collection of the envelopes in my studio. Each unique pattern gets scanned and stored on my computer and cataloged on my are.na board, 'envelope liners'."
     },
     {
         title:"A collection of Naoto's memories",
         author:"Naoto Hieda",
         range:"2022",
         link:"https://soup.glitches.me/",
-        method:""
+        method:"using various modalities (text, photos and songs, etc) and exhibiting with a custom made reactive webpage"
     },
     {
         title:"A collection of every officially licensed mew and mewtwo plush",
         author:"Kyle Robateau",
         range:"2024 – present",
         link:"collections/robateau-plushes.pdf",
-        method:""
+        method:"I organize it in a google speadsheet, cross-referencing info on various websites and copying + pasting images."
     },
     {
         title:"A collection of signs telling us what not to do around the world",
         author:"Laura Sinisterra",
         range:"2022 – present",
         link:"https://www.are.na/laura-sinisterra/observing-signs",
-        method:""
+        method:"taking pictures every time I see an interesting sign, with a particular focus on handwritten, then I categorize them into a folder in my phone, then upload them to are.na"
     },
     {
         title:"A collection of stills from Ratatouille (2007)",
         author:"eastmountain",
         range:"May 2021",
         link:"./collections/eastmountain.html",
-        method:""
+        method:"screenshotting and saving to a folder"
     },
     {
         title:"A collection of every data graphic in Star Trek",
         author:"Emily Fuhrman",
         range:"2016-2021",
         link:"https://startrekvis.tumblr.com/",
-        method:""
+        method:"I watched every movie and episode of Star Trek (excluding the newest series, though I made a valiant effort on the first seasons of each) and paused to screenshot any data graphics that appeared."
     },
     {
         title:"A collection of floor plans",
         author:"Emily Fuhrman",
         range:"May 2014-2016",
         link:"https://emilyfuhrman.co/interactive/Y2014002/",
-        method:""
+        method:"Photographing floor plans in the wild, usually seen next to elevators."
     },
     {
       title:"A collection of scans from my mom and uncle's stamp collection",
       author:"Vidya Giri",
       range:"1960s-1990s",
       link:"https://stamps.vidyagiri.com/",
-      method:""
+      method:"my uncle and aunt collected them in a stamp album when they were kids. i scanned, sorted, and edited them and created a website to share the collection and related links I used in order to write the captions!"
     },
     {
       title:"A collection of dudes with doves",
       author:"Karla Krey",
       range:"2023–present",
       link:"https://www.are.na/karla-krey/dudes-with-doves",
-      method:""
+      method:"The collection consists mostly of screenshots or downloaded image files from the web. Sometimes I search specifically, e.g. for racing pigeon clubs, sometimes they pop up in my social media feed."
     },
     {
       title:"A collection of intentional and stream of consciousness lists for various aspects of my life",
       author:"Amaryllis Ara",
       range:"1960s-1990s",
       link:"",
-      method:""
+      method:"In my notes app. I continuously add to ongoing lists and intuitively create lists based on what pops into my head at the moment."
     },
     {
       title:"A collection of sacred invocations and memefied musings on the internet space.",
       author:"Prayer on the Internet",
       range:"2023-present",
       link:"https://www.are.na/marshall-wang/prayer-on-the-internet",
-      method:""
+      method:"Are.na is the main tool I use to collect them. I put everything I discover in the folder. Sometimes I screenshot and upload, sometimes I collect them through the browser, and sometimes I find them on social media."
     },
     {
       title:"A collection of urinals I've used.",
       author:"Ashton Reeder",
       range:"2023-present",
       link:"",
-      method:""
+      method:"photo of every urinal i've used in the last 3 years."
     },
     {
       title:"A collection of pictures of computer screens",
       author:"Rachael Previti",
       range:"2024-present",
       link:"https://drive.google.com/drive/folders/1zfa1hbGTCpfltIM-NQ_qmXV-SM0eU1gm?usp=sharing",
-      method:""
+      method:"taking pictures of things on my computer and saving them to a folder on my phone!"
     },
     {
       title:"A collection of typography",
       author:"@type.gram",
       range:"2019-present",
       link:"https://www.instagram.com/type.gram/",
-      method:""
+      method:"Usually by taking photos of irl type using my phone, or taking a screenshot on my phone or computer — saving to a folder in my iphone with some random emoji to let me know it's my type collection — then I share it to my public account, @type.gram on instagram. Much documented type does not make it that far due to me being forgetful, but nonetheless, it is still appreciated :)"
     },
     {
       title:"A collection of lost Chinese feminist websites from before social media took over - kind of like digital ghosts of what feminist spaces on the Chinese internet used to look like in the early 2000s",
       author:"ephemeral:data\ (Crassula Shang, i-want-cookies, and YoEve)",
       range:"2023-present",
       link:"https://chinese-cyberfeminism-archive.com/",
-      method:""
+      method:"we're like digital archaeologists with a bit of a DIY approach: - we dig through the Wayback Machine looking for traces of these old sites - we wrote our own web crawler to help us gather snapshots and images (it's kind of scrappy, but it works!) - when we find one site, we follow all its links to discover others (like following a trail of breadcrumbs through internet history) - we put all sites in a hand-coded web archive with embedded web emulators so people can actually click around these old sites"
     },
     {
       title:"A collection of visual fragments - diagrams, images, and graphic elements - gathered from materials in the Cybernetics Library",
       author:"cha.ski",
       range:"2024-present",
       link:"https://cyberneticsimagelibrary.netlify.app/",
-      method:""
+      method:"An open, living archive where visitors submit images - often screenshots or cropped phone photos from physical or digitized sources - of visual fragments - diagrams, photographs, and graphic elements - gathered from materials in the Cybernetics Library catalog. Visitors can traverse the ever-evolving visual repository through search, filtering, collections, and associative tags, traversing relationships and encountering latent associations within the growing image dataset. Visitors submit images through a form, which automatically stores them in Firebase, catalogs image and resource metadata in Google Sheets, and retrieves them dynamically (via Firestore) on the web platform built with Vue.js. The Cybernetics Library’s expansive collection of over 2,000 materials ranges from media theory, cognitive science, and architecture to grassroots zines, rare archival material and ephemera, artists’ publications and technical literature. "
     },
     {
       title:"A collection of songs that have captured the vibes of specific days in my life",
       author:"mk zariel",
       range:"2025 onward",
       link:"https://open.spotify.com/playlist/5EVVes9QqhUriZ8NiCM6hb?si=b8927efd96f844de",
-      method:""
+      method:"at the end of every day, i reflect on my experiences that day and choose a song that feels resonant—sometimes on content, but often just based on vibes—and add to this playlist."
     },
     {
       title:"A collection of people selling old tvs",
       author:"Kiko_ Tv for sale",
       range:"2021-present",
       link:"https://www.are.na/gio-kikoria-5ofhwxzjrc0/ree40esscps",
-      method:""
+      method:"facebook marketplace"
     },
     {
       title:"A collection of good morning images and quotes from my family WhatsApp groups",
       author:"Marina Cardoso",
       range:"2020-present",
       link:"",
-      method:""
+      method:"downloading the files and saving them to a folder on my computer"
     },
     {
       title:"A collection of personal episode rankings and data points for Star Trek: The Next Generation",
       author:"Lucia Gomez",
       range:"2024-present",
       link:"https://deciduous-ring-e37.notion.site/10b00f12549080e08d90c0ddc00bdeac?v=10b00f12549080788a10000c93ce9250",
-      method:""
+      method:"watching an episode and then adding an entry to my Notion table"
     },
     {
       title:"A collection of notes app drawings that i drew with my finger while on long phone conversations—primarily with my friend malia.",
       author:"christine (cynical pinay)",
       range:"2020-present",
       link:"https://drive.google.com/drive/folders/1-Yz2j05CI_Co0ysXwYHIlJe5LsDtE2f9?usp=sharing",
-      method:""
+      method:"they are collected on non-written notes and within random written notes in my iphone. multiple drawings are also collected within one note. for the purposes of this submission, they are screenshotted and filed into a google drive folder."
     },
     {
       title:"A collection of google maps user submitted photos of grocery stores",
       author:"Vanessa Anderson",
       range:"2024-present",
       link:"https://grocerygoblin.substack.com/p/25-grocery-stores",
-      method:""
+      method:"screenshotting"
     },
     {
       title:"A collection of 11:11",
       author:"Amelia K.",
       range:"2022-present",
       link:"",
-      method:""
-    },
-    {
-      title:"A collection of google maps user submitted photos of grocery stores",
-      author:"Vanessa Anderson",
-      range:"2024-present",
-      link:"https://grocerygoblin.substack.com/p/25-grocery-stores",
-      method:""
+      method:"screenshots"
     },
     {
       title:"A collection of internet comments",
       author:"Spencer Chang",
       range:"2024-present",
       link:"https://acknowledge.our-inter.net/",
-      method:""
+      method:"i copy a link to the comment and submit it to a form, i have browser automation code i made on my computer that i then run to scrape the data from the link and upload it to the collection where it shows up on the website"
     },
     {
       title:"A collection of typefaces and fonts I've used for many projects over the years. Some are good, some are bad, and some are just ok.",
       author:"Jonathan Sangster",
       range:"2021-2025",
       link:"https://drive.google.com/drive/folders/1Oqg2mfic30gx6S7lfeWfA-qosVWYhhGB?usp=sharing",
-      method:""
-    }
+      method:"I use internet research to collect these for current and future use or as a pathway to inspiration."
+    },
+    {
+      title:"A collection of missed connections from craigslist",
+      author:"Grace Wong",
+      range:"2018-present",
+      link:"https://www.are.na/grace-wong-1523316133/ghost-lovers",
+      method:"are.na channel"
+    },
+    {
+      title:"A collection of satellite spaces that have led to the disintegration of Chinatowns in the name of urban renewal",
+      author:"Grace Wong",
+      range:"2021",
+      link:"https://www.are.na/grace-wong-1523316133/satellite-spaces-that-have-led-to-the-disintegration-of-chinatowns-in-the-name-of-urban-renewal",
+      method:"are.na channel"
+    },
+    {
+      title:"A collection of couples things",
+      author:"Grace Wong",
+      range:"2022–present",
+      link:"https://www.are.na/grace-wong-1523316133/couples-things",
+      method:"are.na channel"
+    },
+    {
+      title:"A collection/archive of strip club signage and ephemera from the pandemic.",
+      author:"Dahlia Bloomstone",
+      range:"2023–present",
+      link:"",
+      method:"I collect by continually searching 'strip club signage and ephemera from the pandemic' every few weeks and screenshotting and saving the images to my desktop in a folder titled 'Little Darlings.' I also edit the images at times. I will submit a few images that represent the collection, with an original image and two edited images."
+    },
+    {
+      title:"A collection of online articles on AI' screeshots",
+      author:"Radomic-Snapshotted: Flânering Readings’ Memoir",
+      range:"2022–present",
+      link:"",
+      method:"screenshotting The New York Times articles on Tech and AI and saving to a folder"
+    },
+    {
+      title:"A collection of Personal Growth Through Art",
+      author:"Shiva Viswanathan",
+      range:"2021–present",
+      link:"https://www.instagram.com/daysofshiva/",
+      method:"Drawings: Sketch on a notebook and take a photo with my phone and posting it to instagram; 3D art: Made the artwork and then used editing software to add music that resonated with my that day and then post it on instgagram"
+    },
+    {
+      title:"A collection of my favorite memes",
+      author:"Kelci D Crawford",
+      range:"2011-2017",
+      link:"",
+      method:"I save the file into a folder on my external hard drive."
+    },
+    {
+      title:"A collection of photos of art that inspired me",
+      author:"Lauren Schroeder",
+      range:"2017–present",
+      link:"https://www.laurenschroeder.com/inspiration",
+      method:"Taking photos with my iphone, and have uploaded to my personal CMS"
+    },
+    {
+      title:"A collection of inspiration sparkers for writing",
+      author:"Djahane",
+      range:"early 2010s–present",
+      link:"",
+      method:"screenshots, photos I took, copy paste the line from an online text (eg: a tumblr post or an academic reading) into a document. everything is sorted into folders and documents depending by their theme/characteristics (eg: character building, world building, story prompts etc...)"
+    },
+    {
+      title:"A collection of things that represent a corner of my brain",
+      author:"A Corner Of My Brain",
+      range:"2024–present",
+      link:"https://www.are.na/nunu/a-corner-of-my-brain-gz5-grsvez8",
+      method:"copying + pasting into are.na channel"
+    },
+    {
+      title:"A collection of Kakaotalk(Korean Whatsup) profile pictures I decorated throughout the years!",
+      author:"JUNOS",
+      range:"2024–2025",
+      link:"",
+      method:"Screenshot and saving it to my photo album"
+    },
+    {
+      title:"A collection of something went wrong messages",
+      author:"Cortney Cassidy",
+      range:"2020–present",
+      link:"https://www.are.na/cortney-cassidy/something-went-wrong",
+      method:"are.na channel"
+    },
+    {
+      title:"A collection of wi-fi names out in the world",
+      author:"dani bloop",
+      range:"2022–present",
+      link:"https://www.are.na/dani-bloop/wi-fi-names-out-in-the-world",
+      method:"are.na channel"
+    },
 
 ]
 
